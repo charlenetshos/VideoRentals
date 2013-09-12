f8ce4d7220366d04b671ffbddfe6d3c17b55b293
Charlene <charlet@thoughtworks.com>
2013-09-12 09:18:50 +0200

    Adding HTML statement

--- src/com/tw/bootcamp/rental/Customer.java
+++ src/com/tw/bootcamp/rental/Customer.java
@@ -64,4 +64,49 @@ class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
+
+    public String htmlStatement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration rentals = _rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+
+        while (rentals.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = (Rental) rentals.nextElement();
+
+            //determine amounts for each line
+            switch (each.getMovie().getPriceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.getDaysRented() > 2)
+                        thisAmount += (each.getDaysRented() - 2) * 1.5;
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.getDaysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.getDaysRented() > 3)
+                        thisAmount += (each.getDaysRented() - 3) * 1.5;
+                    break;
+            }
+
+            // add frequent renter points
+            frequentRenterPoints++;
+
+            // add bonus for a two day new release rental
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
+                frequentRenterPoints++;
+
+            //show figures for this rental
+            result += each.getMovie().getTitle() + ": " + String.valueOf(thisAmount) + "<BR>\n";
+            totalAmount += thisAmount;
+        }
+
+        //add footer lines
+        result += "<P>You owed <EM>" + String.valueOf(totalAmount) + "</EM><P>\n";
+        result += "On this rental you earned <EM>" + String.valueOf(frequentRenterPoints) + "</EM> frequent renter points<P>";
+        return result;
+    }
 }
--- test/com/tw/bootcamp/rental/CustomerTest.java
+++ test/com/tw/bootcamp/rental/CustomerTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertEquals;
 
 public class CustomerTest {
     @Test
-    public void shouldPrintCorrectStatement() {
+    public void shouldPrintTextStatement() {
         Customer customer = new Customer("Krishna Sure");
         int[] daysRented = {4, 3, 2, 5, 1};
 
@@ -34,5 +34,33 @@ public class CustomerTest {
                 "You earned 6 frequent renter points";
 
         assertEquals(expectedResult, customer.statement());
+    }
+
+    @Test
+    public void shouldPrintHTMLStatement() {
+        Customer customer = new Customer("Krishna Sure");
+        int[] daysRented = {4, 3, 2, 5, 1};
+
+        ArrayList<Movie> movieArrayList = new ArrayList<>();
+        movieArrayList.add(new Movie("102 Dalmatiens", Movie.CHILDRENS));
+        movieArrayList.add(new Movie("She's Out Of My League", Movie.REGULAR));
+        movieArrayList.add(new Movie("Scooby-Doo and the Alien Invaders", Movie.CHILDRENS));
+        movieArrayList.add(new Movie("Despicable Me 2", Movie.NEW_RELEASE));
+        movieArrayList.add(new Movie("Salt", Movie.REGULAR));
+
+        for (int i = 0; i < movieArrayList.size(); i++) {
+            customer.addRental(new Rental(movieArrayList.get(i), daysRented[i]));
+        }
+
+        String expectedResult = "<H1>Rentals for <EM>Krishna Sure</EM></H1><P>\n" +
+                "102 Dalmatiens: 3.0<BR>\n" +
+                "She's Out Of My League: 3.5<BR>\n" +
+                "Scooby-Doo and the Alien Invaders: 1.5<BR>\n" +
+                "Despicable Me 2: 15.0<BR>\n" +
+                "Salt: 2.0<BR>\n" +
+                "<P>You owed <EM>25.0</EM><P>\n" +
+                "On this rental you earned <EM>6</EM> frequent renter points<P>";
+
+        assertEquals(expectedResult, customer.htmlStatement());
     }
 }
